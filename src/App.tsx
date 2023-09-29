b1e3b5a9830b38eb7f9ecc91d46ad8ddf84cc500
Koustubh Moharir <koustubhmoharir@gmail.com>
2023-09-29 20:35:55 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -92,8 +92,14 @@ const pages: Page[] = [
       <ul>
         <li>
           At the end of the sprint, QA will be released to UAT. This commit
-          should be tagged and new sprint will begin with feature branches from
-          this UAT
+          should be tagged cli-UAT-yyyy-mm-dd and new sprint will begin with
+          feature branches from this UAT
+        </li>
+        <li>
+          Whenever a signoff is received for this release to UAT, it will be
+          merged (fast-forward) into Prod. The tag helps with locating this
+          commit, because newer sprints might be merged into UAT before the
+          signoff, moving the UAT branch forward.
         </li>
       </ul>
     ),
@@ -120,6 +126,118 @@ const pages: Page[] = [
       },
       f3_0: { x: 0, from: "q3", names: ["cl1-QA", "cl1-F3"], desc: "F3: ..." },
       f4_0: { x: 1, from: "q3", names: ["cl1-F4"], desc: "F4: ..." },
+    },
+  },
+  {
+    desc: (
+      <ul>
+        <li>When an issue is encountered, a branch will be created on Prod</li>
+      </ul>
+    ),
+    commits: {
+      c0: { x: 0, names: [], desc: "initial" },
+      f1_0: { x: 0, from: "c0", names: [], desc: "F1: ..." },
+      f1_1: { x: 0, from: "f1_0", names: [], desc: "F1: ..." },
+      f2_0: { x: 2, from: "c0", names: [], desc: "F2: ..." },
+      f2_1: { x: 2, from: "f2_0", names: ["cl1-F2"], desc: "F2: ..." },
+      q2: {
+        x: 1,
+        from: "f2_1",
+        from2: "f1_1",
+        names: [],
+        desc: "Merged F2 into QA",
+      },
+      f1_2: { x: 0, from: "f1_1", names: ["cl1-F1"], desc: "F1: ..." },
+      q3: {
+        x: 0,
+        from: "q2",
+        from2: "f1_2",
+        names: ["cl1-UAT-2023-09-01", "cl1-Prod"],
+        desc: "Merged F1 into QA",
+      },
+      f3_0: { x: 0, from: "q3", names: ["cl1-F3"], desc: "F3: ..." },
+      f4_0: { x: 1, from: "q3", names: ["cl1-F4"], desc: "F4: ..." },
+      q4: {
+        x: 0,
+        from: "f3_0",
+        from2: "f4_0",
+        names: ["cl1-UAT-2023-09-15", "cli-UAT"],
+        desc: "Merged F4 into QA",
+      },
+      f5_0: { x: 0, from: "q4", names: ["cl1-F5", "cl1-QA"], desc: "F5: ..." },
+      i1_0: { x: 2, from: "q3", names: [], desc: "I1: ..." },
+      i1_1: { x: 2, from: "i1_0", names: ["cl1-I1"], desc: "I1: ..." },
+    },
+  },
+  {
+    desc: (
+      <ul>
+        <li>
+          The issue branch will be merged into UAT and / or QA, tested
+          internally, then deployed to client awaiting signoff
+        </li>
+        <li>
+          When a signoff is received for deploying the issue, it will be merged
+          into Prod
+        </li>
+      </ul>
+    ),
+    commits: {
+      c0: { x: 0, names: [], desc: "initial" },
+      f1_0: { x: 0, from: "c0", names: [], desc: "F1: ..." },
+      f1_1: { x: 0, from: "f1_0", names: [], desc: "F1: ..." },
+      f2_0: { x: 2, from: "c0", names: [], desc: "F2: ..." },
+      f2_1: { x: 2, from: "f2_0", names: ["cl1-F2"], desc: "F2: ..." },
+      q2: {
+        x: 1,
+        from: "f2_1",
+        from2: "f1_1",
+        names: [],
+        desc: "Merged F2 into QA",
+      },
+      f1_2: { x: 0, from: "f1_1", names: ["cl1-F1"], desc: "F1: ..." },
+      q3: {
+        x: 0,
+        from: "q2",
+        from2: "f1_2",
+        names: ["cl1-UAT-2023-09-01"],
+        desc: "Merged F1 into QA",
+      },
+      f3_0: { x: 0, from: "q3", names: ["cl1-F3"], desc: "F3: ..." },
+      f4_0: { x: 1, from: "q3", names: ["cl1-F4"], desc: "F4: ..." },
+      q4: {
+        x: 0,
+        from: "f3_0",
+        from2: "f4_0",
+        names: ["cl1-UAT-2023-09-15"],
+        desc: "Merged F4 into QA",
+      },
+      f5_0: { x: 0, from: "q4", names: ["cl1-F5", "cl1-QA"], desc: "F5: ..." },
+      i1_0: { x: 2, from: "q3", names: [], desc: "I1: ..." },
+      i1_1: { x: 2, from: "i1_0", names: ["cl1-I1"], desc: "I1: ..." },
+      ui1: {
+        x: 1,
+        from: "i1_1",
+        from2: "q4",
+        names: [],
+        desc: "Merged I1 into UAT",
+      },
+      i2_0: { x: 3, from: "q3", names: [], desc: "I2: ..." },
+      i2_1: { x: 3, from: "i2_0", names: ["cl1-I2"], desc: "I2: ..." },
+      ui2: {
+        x: 1,
+        from: "i2_1",
+        from2: "ui1",
+        names: ["cl1-UAT"],
+        desc: "Merged I2 into UAT",
+      },
+      p1: {
+        x: 2,
+        from: "i1_1",
+        from2: "i2_1",
+        names: ["cl1-Prod"],
+        desc: "Merged I2 into Prod",
+      },
     },
   },
 ];
@@ -173,7 +291,9 @@ function Page({ index }: { index: number }) {
           />
         ) : (
           <path
-            d={`M ${pt(p1)} L ${pt([p2[0], p1[1]])} L ${pt(p2)}`}
+            d={`M ${pt(p1)} C ${pt([p2[0], p1[1]])} ${pt([p2[0], p1[1]])} ${pt(
+              p2,
+            )}`}
             fill="none"
             stroke="currentColor"
             opacity={opacity}
